bde2d962ed112b3a76f25ee3e6ecfbe66ebad9e2
timothy-20 <jungun3329@gmail.com>
2021-03-09 18:09:55 +0900

    picker view 첫 구현

--- TableList0308/Modal/AddModalViewController.m
+++ TableList0308/Modal/AddModalViewController.m
@@ -8,13 +8,18 @@
 #import "AddModalViewController.h"
 #import "LabelViewController.h"
 
-@interface AddModalViewController ()<UITableViewDelegate, UITableViewDataSource>
+@interface AddModalViewController ()<UITableViewDelegate, UITableViewDataSource, UIPickerViewDelegate, UIPickerViewDataSource>
 {
     NSMutableArray *cellLabel;
     NSInteger selectedIndex;
+    
+    NSArray *data1;
+    NSArray *data2;
 }
 
 @property (weak, nonatomic) IBOutlet UITableView *modalTable;
+@property (weak, nonatomic) IBOutlet UIPickerView *uiPicker;
+
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *coffeeCancel;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *coffeeAdd;
 
@@ -33,36 +38,19 @@
     
     self.modalTable.delegate = self;
     self.modalTable.dataSource = self;
+    
+    data1 = [NSArray arrayWithObjects:@"data1", @"data2", @"data3", @"data4", nil];
+    data2 = [NSArray arrayWithObjects:@"test1", @"test2", @"test3", @"test4", nil];
+    
+    self.uiPicker.delegate = self;
+    self.uiPicker.dataSource = self;
 }
+
+#pragma mark - custom cell
 
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     return 1;
-}
-
-//-(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
-//{
-//    UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, 18)];
-//    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(10, 5, tableView.frame.size.width, 18)];
-//
-//    [label setFont:[UIFont boldSystemFontOfSize:12]];
-//    NSString *string = [cellLabel objectAtIndex:section];
-//
-//    [label setText:string];
-//    [headerView addSubview:label];
-//    [headerView setBackgroundColor:[UIColor redColor]];
-//
-//    return headerView;
-//}
-
--(UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section
-{
-    UIView *footerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, 1)];
-    
-    //footerView.layer.borderColor = [[UIColor grayColor] CGColor];
-    //footerView.layer.borderWidth = 1.0;
-    
-    return footerView;
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -79,10 +67,11 @@
     return cell;
 }
 
+#pragma mark - tablecell selection
+
 -(NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     selectedIndex = indexPath.row;
-    NSLog(@"%d", indexPath.row);
     return indexPath;
 }
 
@@ -104,11 +93,81 @@
     
 }
 
-- (IBAction)coffeeAddAction:(id)sender {
+#pragma mark - section style
+
+-(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, 18)];
+    
+//    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(10, 5, tableView.frame.size.width, 18)];
+//
+//    [label setFont:[UIFont boldSystemFontOfSize:12]];
+//    NSString *string = [cellLabel objectAtIndex:section];
+//
+//    [label setText:string];
+//    [headerView addSubview:label];
+    
+    [headerView setBackgroundColor:[UIColor grayColor]];
+
+    return headerView;
+}
+
+-(UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section
+{
+    UIView *footerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, 0)];
+    
+//    footerView.layer.borderColor = [[UIColor grayColor] CGColor];
+//    footerView.layer.borderWidth = 1.0;
+    
+    [footerView setBackgroundColor:[UIColor grayColor]];
+    
+    return footerView;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    return 1;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 1;
+}
+
+#pragma mark - picker view
+
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 2;
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    if(component == 0) {
+        return data1.count;
+    } else {
+        return data2.count;
+    }
+}
+
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    if(component == 0) {
+        return [data1 objectAtIndex:row];
+    } else {
+        return [data2 objectAtIndex:row];
+    }
+}
+
+#pragma mark - navigation button action
+
+- (IBAction)coffeeAddAction:(id)sender
+{
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-- (IBAction)coffeeCancelAction:(id)sender {
+- (IBAction)coffeeCancelAction:(id)sender
+{
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 @end
